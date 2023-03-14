a117b988a62d9bc4083f89647a8bd64edb2a0a2c
xiannu-nicole <nicole16312@gmail.com>
2023-03-14 09:56:25 +0800

    <config 2>

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,18 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/portfolios/'
-      : '/'
+    // publicPath: process.env.NODE_ENV === 'production'
+    // ? '/portfolios/'
+    // : '/',
+    productionSourceMap: false,
+    publicPath: './',
+    outputDir: 'dist',
+    assetsDir: 'assets',
+    devServer: {
+        port: 8090,
+        host: '0.0.0.0',
+        https: false,
+        open: true
+    },
 }
+
+
+    
